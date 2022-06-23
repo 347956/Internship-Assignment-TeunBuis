a0b892c51537f0efb5838dedd40856c954de4256
Teun <teunbuis17@gmail.com>
2022-06-24 00:25:26 +0200

    Working on the ServiceBus

---
+++ Internship-Assignment-Teunbuis/Internship-Assignment-Teunbuis/Services/MessageQueue.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.ServiceBus;
+
+namespace Internship_Assignment_Teunbuis.Services
+{
+    public class MessageQueue
+    {
+        private readonly IConfiguration config;
+
+        public MessageQueue(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public async Task SendMessageAsync<T>(T ServiceBusMessage, string queueName)
+        {
+            var queueClient = new QueueClient(config.GetConnectionString("AzureServiceBus"), queueName);
+        }
+    }
+}
