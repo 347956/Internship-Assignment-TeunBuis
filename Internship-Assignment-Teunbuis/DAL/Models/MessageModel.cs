e27eebb1ba0b50b3bccadb9e6be733f688e29ac0
Teun <teunbuis17@gmail.com>
2022-06-23 22:51:55 +0200

    Initial Commit

---
+++ Internship-Assignment-Teunbuis/DAL/Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Internship-Assignment-Teunbuis/Internship-Assignment-Teunbuis/Controllers/MessageController.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity.Validation;
+
+namespace Internship_Assignment_Teunbuis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        private readonly DataContext dataContext;
+        public MessageController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MessageModel>>> get()
+        {
+            List<MessageModel> messageModels = await dataContext.Messages.ToListAsync();
+            return Ok(messageModels.OrderByDescending(messageModel => messageModel.Date));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostMessage(MessageModel messageModel)
+        {
+            messageModel = CheckMessage(messageModel);
+            try
+            {                
+                dataContext.Messages.Add(messageModel);
+                await dataContext.SaveChangesAsync();
+            }
+            catch(DbEntityValidationException e)
+            {
+                Console.WriteLine(e);
+            }
+            return Ok();
+        }
+
+        private MessageModel CheckMessage(MessageModel messageModel)
+        {
+            if(messageModel.UserName == null)
+            {
+                messageModel.UserName = "Anymous";
+            }
+            if(messageModel.Content == null)
+            {
+                messageModel.Content = "N/A";
+            }
+            if(messageModel.Date == null)
+            {
+                messageModel.Date = DateTime.Now;
+            }
+            return messageModel;
+        }
+    }
+}
