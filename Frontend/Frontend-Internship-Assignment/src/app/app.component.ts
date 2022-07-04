cb256dda94c2b166252d61a11263d71bac41f79e
Teun <teunbuis17@gmail.com>
2022-07-04 13:13:40 +0200

    Front-end websocket setup

--- Frontend/Frontend-Internship-Assignment/src/app/app.component.ts
+++ Frontend/Frontend-Internship-Assignment/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Frontend-Internship-Assignment';
+  constructor(private webSocketService: WebsocketService){
+
+  }
+  ngOnInit(){
+    this.webSocketService.listen('newmessagequeue').subscribe((data) =>{
+      console.log(data);
+    });
+  }
 }
---
+++ Frontend/Frontend-Internship-Assignment/src/app/websocket.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable, Subscriber } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  socket: any;
+  readonly uri: string = "ws://localhost:7032/Message";
+  constructor() {
+    this.socket = io.io(this.uri);
+   }
+
+  listen(eventName: string){
+    return new Observable((subscriber) =>{
+      this.socket.on(eventName, (data: any) =>{
+        subscriber.next(data);
+      })
+    });
+  }
+
+  emit(eventName: string, data: any){
+    this.socket.emit(eventName, data);
+  }
+
+}
